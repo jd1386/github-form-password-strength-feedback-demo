adcc6f2c0f7932efd53bbb1dcc4feab79fb1bce6
Jungdo Lee <lee.jungdo@gmail.com>
2019-02-07 22:15:27 +0900

    Finish project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import zxcvbn from 'zxcvbn';
 
 class App extends Component {
+  state = {
+    passwordScore: ''
+  };
+
+  handleOnPasswordInput(passwordInput) {
+    console.log(zxcvbn(passwordInput));
+    const { score } = zxcvbn(passwordInput);
+    this.setState({ passwordScore: score });
+  }
+
+  renderFeedbackMessage() {
+    const { passwordScore } = this.state;
+    let message, className;
+
+    switch (passwordScore) {
+      case 0:
+        message = 'Way too weak!';
+        className = 'text-danger';
+        break;
+      case 1:
+        message = 'Weak strength!';
+        className = 'text-danger';
+        break;
+      case 2:
+        message = 'Moderate strength!';
+        className = 'text-warning';
+        break;
+      case 3:
+        message = 'Good strength!';
+        className = 'text-success';
+        break;
+      case 4:
+        message = 'Powerful strength!';
+        className = 'text-primary';
+        break;
+      default:
+        message = '';
+        break;
+    }
+
+    return (
+      <small id="passwordHelp" className={`form-text mt-2 ${className}`}>
+        {`${message}`}
+      </small>
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form className="my-form">
+          <div className="form-row">
+            <div className="col-md-12 mb-3">
+              <label htmlFor="passwordInput">패스워드</label>
+              <input
+                type="password"
+                className="form-control"
+                id="passwordInput"
+                onChange={e => this.handleOnPasswordInput(e.target.value)}
+              />
+              {this.renderFeedbackMessage()}
+            </div>
+          </div>
+          <button type="submit" className="btn btn-primary btn-block">
+            Submit
+          </button>
+        </form>
       </div>
     );
   }
